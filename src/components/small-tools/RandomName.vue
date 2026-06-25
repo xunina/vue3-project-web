eb1be4c6fb8cafc518175b2cf01e2e6eb421d79a
nina <1364532500@qq.com>
2026-06-25 17:09:20 +0800

    add random name

---
+++ src/components/small-tools/RandomName.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <label>随即昵称生成：</label>
+    <el-input v-model="chineseName" style="width: 240px" placeholder="Please input" />
+    <el-button @click="clickRefresh"> 生成 </el-button>
+    <el-button @click="clickCopy"> 复制 </el-button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { CHINA_NAME, CHINA_CHAR } from '@/constants/common'
+
+const chineseName = ref<string>('')
+
+const getNameFirst = (): string => {
+  const index = Math.floor(Math.random() * CHINA_NAME.length)
+  return CHINA_NAME[index] || ''
+}
+
+const getNameSecond = (): string => {
+  const index = Math.floor(Math.random() * CHINA_CHAR.length)
+  return CHINA_CHAR[index] || ''
+}
+
+const getAllName = () => {
+  return getNameFirst() + getNameSecond() + getNameSecond()
+}
+
+chineseName.value = getAllName()
+
+const clickRefresh = () => {
+  chineseName.value = getAllName()
+}
+
+const clickCopy = () => {
+  navigator.clipboard
+    .writeText(chineseName.value)
+    .then(() => {
+      ElMessage({
+        message: '已成功复制到剪贴板！',
+        type: 'success',
+      })
+    })
+    .catch((err) => {
+      console.error('复制失败:', err)
+      ElMessage.error('复制失败，请重试')
+    })
+}
+</script>
+<style lang="css" scoped></style>
---
+++ src/views/small-tools/RandomGenerateView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <RandomName></RandomName>
+  </div>
+</template>
+<script setup lang="ts">
+import RandomName from '@/components/small-tools/RandomName.vue'
+</script>
+<style lang="css" scoped></style>
